25215b48a876d18a5cc3015ef5e207023c7efb8f
Davi <davilopeszin@gmail.com>
2026-09-13 18:16:21 -0300

    rabbit test properties and mockbean for rabbit connection

--- order-producer-service/src/test/java/com/example/orderproducerservice/OrderProducerServiceApplicationTests.java
+++ order-producer-service/src/test/java/com/example/orderproducerservice/OrderProducerServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.example.orderproducerservice;
 
+import com.rabbitmq.client.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OrderProducerServiceApplicationTests {
+
+    @MockitoBean
+    private ConnectionFactory connectionFactory;
 
     @Test
     void contextLoads() {
