3b3f681d9e96cd5768b6ea6e170d9b0ce1c281b6
Davi <davilopeszin@gmail.com>
2026-09-13 17:56:16 -0300

    h2 database for CI tests

--- order-producer-service/src/test/java/com/example/orderproducerservice/OrderProducerServiceApplicationTests.java
+++ order-producer-service/src/test/java/com/example/orderproducerservice/OrderProducerServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.orderproducerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderProducerServiceApplicationTests {
 
     @Test
