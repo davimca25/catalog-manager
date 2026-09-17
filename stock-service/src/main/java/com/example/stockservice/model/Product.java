73f702d9b965edb1d6e4d98ba323e3e0ec5e5bd7
davil <davilopeszin@gmail.com>
2026-09-17 01:13:40 -0300

    feat(stock): implements productRepository, rabbitMQConfig and model

---
+++ stock-service/src/main/java/com/example/stockservice/model/Product.java
@@ -0,0 +1,27 @@
+package com.example.stockservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_products")
+public class Product {
+
+    @Id
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ stock-service/src/main/java/com/example/stockservice/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.stockservice.model;
+
+public enum Status {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
