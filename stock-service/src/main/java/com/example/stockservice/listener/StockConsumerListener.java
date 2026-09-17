549df5c3bcf3d747611fc9bfeba1bfc6f7467327
davil <davilopeszin@gmail.com>
2026-09-17 12:24:25 -0300

    feat(stock): minor spaces

--- stock-service/src/main/java/com/example/stockservice/listener/StockConsumerListener.java
+++ stock-service/src/main/java/com/example/stockservice/listener/StockConsumerListener.java
@@ -2,15 +2,12 @@ package com.example.stockservice.listener;
 
 import com.example.stockservice.dto.OrderEventDTO;
 import com.example.stockservice.dto.OrderEventDTO.OrderItemEventDTO;
-import com.example.stockservice.model.Product;
 import com.example.stockservice.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,7 +20,10 @@ public class StockConsumerListener {
     @Transactional
     public void receiveStockEvent(OrderEventDTO orderEventDTO) {
 
-        log.info("Message received from stock queue: OrderId = {}, UserName = {}", orderEventDTO.orderId(), orderEventDTO.userName());
+        log.info("Message received from stock queue: OrderId = {}, UserName = {}",
+                orderEventDTO.orderId(),
+                orderEventDTO.userName()
+        );
 
         for (OrderItemEventDTO orderItemEventDTO : orderEventDTO.items()) {
 
@@ -35,7 +35,11 @@ public class StockConsumerListener {
                     productRepository.save(product);
 
                 } else {
-                    log.warn("Insufficient stock for ProductId = {}. Current Stock = {}, Requested = {}", product.getId(), product.getQuantity(), orderItemEventDTO.quantity());
+                    log.warn("Insufficient stock for ProductId = {}. Current Stock = {}, Requested = {}",
+                            product.getId(),
+                            product.getQuantity(),
+                            orderItemEventDTO.quantity()
+                    );
 
                 }
 
