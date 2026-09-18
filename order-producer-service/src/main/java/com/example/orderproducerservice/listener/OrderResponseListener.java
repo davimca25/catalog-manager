c2d6c33ffac2952523cfa001b1f81f11b4d5aa05
davil <davilopeszin@gmail.com>
2026-09-18 17:33:22 -0300

    feat(order): new listener OrderResponseListener for order.response.queue

---
+++ order-producer-service/src/main/java/com/example/orderproducerservice/listener/OrderResponseListener.java
@@ -0,0 +1,43 @@
+package com.example.orderproducerservice.listener;
+
+import com.example.orderproducerservice.dto.OrderEventDTO;
+import com.example.orderproducerservice.dto.OrderEventDTO.OrderItemEventDTO;
+import com.example.orderproducerservice.model.Status;
+import com.example.orderproducerservice.repository.OrderRepository;
+import com.example.orderproducerservice.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderResponseListener {
+
+    private final OrderRepository orderRepository;
+
+    @RabbitListener(queues = "${rabbitmq.queue.order.response.name:order.response.queue}")
+    @Transactional
+    public void receiveOrderResponseEvent(OrderEventDTO orderEventDTO) {
+
+        log.info("Message received from order response queue: OrderId = {}, Status = {}",
+                orderEventDTO.orderId(),
+                orderEventDTO.status()
+        );
+
+        orderRepository.findById(orderEventDTO.orderId()).ifPresentOrElse(order -> {
+
+            order.setStatus(orderEventDTO.status());
+            orderRepository.save(order);
+
+            log.info("Order status updated successfully in DB: OrderId = {}, New Status = {}",
+                    order.getId(),
+                    order.getStatus()
+            );
+
+        }, () -> log.error("Order not found in order-producer-service: OrderId = {}", orderEventDTO.orderId()));
+
+    }
+}
