4e845baf1be223e77809cf2a2680d3f9c1a93347
davil <davilopeszin@gmail.com>
2026-09-18 19:59:49 -0300

    feat(order): new listener product.stock queue

--- order-producer-service/src/main/java/com/example/orderproducerservice/listener/ProductStockSyncListener.java
+++ order-producer-service/src/main/java/com/example/orderproducerservice/listener/ProductStockSyncListener.java
@@ -1,11 +1,27 @@
 package com.example.orderproducerservice.listener;
 
+import com.example.orderproducerservice.dto.ProductStockSyncDTO;
+import com.example.orderproducerservice.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProductStockSyncListener {
+
+    private final ProductRepository productRepository;
+
+    @RabbitListener(queues = "${rabbitmq.queue.product.stock.name:product.stock.queue}")
+    @Transactional
+    public void receiveProductStockSyncEvent(List<ProductStockSyncDTO> products) {
+        log.info("Product received from stock, {} items.", products.size());
+
+
+    }
 }
