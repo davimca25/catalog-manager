87b336fcb7927e877197fed70096e67baf45b655
davil <davilopeszin@gmail.com>
2026-09-22 08:43:35 -0300

    fix(stock): fix OrderEventDTO indentation

--- stock-service/src/main/java/com/example/stockservice/dto/OrderEventDTO.java
+++ stock-service/src/main/java/com/example/stockservice/dto/OrderEventDTO.java
@@ -8,11 +8,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record OrderEventDTO(UUID orderId,
-                            String userName,
-                            Status status,
-                            LocalDateTime createdAt,
-                            List<OrderItemEventDTO> items
+public record OrderEventDTO(
+        UUID orderId,
+        String userName,
+        Status status,
+        LocalDateTime createdAt,
+        List<OrderItemEventDTO> items
 ) implements Serializable {
     public record OrderItemEventDTO(
             UUID productId,
